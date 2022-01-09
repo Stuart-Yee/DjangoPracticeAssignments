66df48e2073b89dbba7146362701894cadbfab69
Stuart Yee <ki6fzb@gmail.com>
2022-01-09 13:12:34 -0800

    game finished!

--- Ninja_Gold/Ninja_Gold/apps/game_app/urls.py
+++ Ninja_Gold/Ninja_Gold/apps/game_app/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^farm$', views.farm),
+    url(r'^cave$', views.cave),
+    url(r'^house$', views.house),
+    url(r'^casino$', views.casino),
+    url(r'^reset$', views.reset),
 ]
--- Ninja_Gold/Ninja_Gold/apps/game_app/views.py
+++ Ninja_Gold/Ninja_Gold/apps/game_app/views.py
@@ -1,14 +1,126 @@
-from django.shortcuts import render, HttpResponse
+import inspect
+
+from django.shortcuts import render, HttpResponse, redirect
 from random import randint
+from time import strftime, localtime
+from functools import wraps
+
+# decorator to run method only if request.method == "POST"
+# Otherwise redirects to "/"
+def post_only(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        for arg in args:
+            try:
+                method = arg.method
+                break
+            except:
+                continue
+        if method == "POST":
+            return f(*args, **kwargs)
+        else:
+            return redirect("/")
+    return inner
+
+
 
 # Create your views here.
+
+
 def index(request):
+    now = strftime("%Y/%m/%d %I:%M %p", localtime())
+
     try:
         gold = request.session["gold"]
     except:
         request.session["gold"] = 0
         gold = 0
 
-    data = {"gold": gold}
+    try:
+        logs = request.session["logs"]
+    except:
+        request.session["logs"] = []
+        logs = []
+        print("Had to create new session")
+
+    data = {
+        "gold": gold,
+        "logs": logs,
+    }
 
     return render(request, "index.html", data)
+
+@post_only # execute only from the button, not from a GET
+def farm(request):
+    # farms produce 10 - 20 gold
+    now = strftime("%Y/%m/%d %I:%M %p", localtime())
+    gold = request.session["gold"]
+    found_gold = randint(10, 20)
+    gold += found_gold
+    request.session["gold"] = gold
+
+    logs = request.session["logs"]
+    log = {"activity": f"Found {found_gold} at the farm! ({now})",
+           "change": "gain"}
+    logs.append(log)
+    request.session["logs"] = logs
+    return redirect("/")
+
+@post_only
+def cave(request):
+    # caves product 5-10 gold
+    now = strftime("%Y/%m/%d %I:%M %p", localtime())
+    gold = request.session["gold"]
+    found_gold = randint(5, 10)
+    gold += found_gold
+    request.session["gold"] = gold
+
+    logs = request.session["logs"]
+    log = {"activity": f"Found {found_gold} in the cave! ({now})",
+           "change": "gain"}
+    logs.append(log)
+    request.session["logs"] = logs
+    return redirect("/")
+
+@post_only
+def house(request):
+    # houses produce 2-5 gold
+    now = strftime("%Y/%m/%d %I:%M %p", localtime())
+    gold = request.session["gold"]
+    found_gold = randint(2, 5)
+    gold += found_gold
+    request.session["gold"] = gold
+
+    logs = request.session["logs"]
+    log = {"activity": f"Found {found_gold} at the house! ({now})",
+           "change": "gain"}
+    logs.append(log)
+    request.session["logs"] = logs
+    return redirect("/")
+
+@post_only
+def casino(request):
+    # you can win or lose 0-50
+    now = strftime("%Y/%m/%d %I:%M %p", localtime())
+    gold = request.session["gold"]
+    found_gold = randint(-50, 50)
+    gold += found_gold
+    request.session["gold"] = gold
+
+    logs = request.session["logs"]
+    if found_gold < 0:
+        log = {"activity": f"Lost {found_gold*-1} at the casino... ouch! ({now})",
+               "change": "loss"}
+    else:
+        log = {"activity": f"Won {found_gold} at the casino! ({now})",
+               "change": "gain"}
+
+    logs.append(log)
+    request.session["logs"] = logs
+    return redirect("/")
+
+@post_only
+def reset(request):
+    request.session["logs"] = []
+    request.session["gold"] = 0
+    return redirect("/")
