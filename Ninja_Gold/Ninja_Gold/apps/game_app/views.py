10e3a5463142872819b00bb71211678529874cb8
Stuart Yee <ki6fzb@gmail.com>
2022-01-08 20:34:25 -0800

    front end complete

--- Ninja_Gold/Ninja_Gold/apps/game_app/views.py
+++ Ninja_Gold/Ninja_Gold/apps/game_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from random import randint
 
 # Create your views here.
 def index(request):
-    data = {"test": "test data"}
+    try:
+        gold = request.session["gold"]
+    except:
+        request.session["gold"] = 0
+        gold = 0
+
+    data = {"gold": gold}
+
     return render(request, "index.html", data)
